bc96029479ab06ab9e35b24076fdc436907cbfc6
Hani <h.samir@edu.salford.ac.uk>
2023-01-28 23:51:25 +0000

    unit test FAILS: tests to render title detail and back button event fail

--- src/components/TitleDetails/TitleDetails.test.tsx
+++ src/components/TitleDetails/TitleDetails.test.tsx
@@ -1,39 +1,57 @@
-import { render } from "@testing-library/react"
-import { Title } from "../../models/Title"
-import TitleDetails from "./TitleDetails"
+import { render, waitFor, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { fetchTitle } from "../../data/dataFetcher";
+import { Title } from "../../models/Title";
+import TitleDetails from "./TitleDetails";
 
-jest.mock('../../data/dataFetcher')
+jest.mock("../../data/dataFetcher");
 
-const result: Title= {
-    titleNumber: "NGL931799",
-    propertyAddress: "Lower Ground Floor, 36-38 Hatton Garden, London (EC1N 8EB)",
-    tenure: "Leasehold",
-    xCoordinate: -0.108098777,
-    yCoordinate: 51.5201911
-}
+const result: Title = {
+  titleNumber: "NGL931799",
+  propertyAddress: "Lower Ground Floor, 36-38 Hatton Garden, London (EC1N 8EB)",
+  tenure: "Leasehold",
+  xCoordinate: -0.108098777,
+  yCoordinate: 51.5201911,
+};
 
-test('should render title details container', () => { 
-    const {container} = render(<TitleDetails/>)
-    const titleContainer = container.getElementsByClassName('titleDetailsContainer')
-    expect(titleContainer.length).toBe(1)
+test("should render title details container", () => {
+  const { container } = render(<TitleDetails />);
+  const titleContainer = container.getElementsByClassName(
+    "titleDetailsContainer"
+  );
+  expect(titleContainer.length).toBe(1);
+});
+
+test("should render title details data on the screen", async () => {
+  const mockTitlesData = jest.mocked(fetchTitle);
+  mockTitlesData.mockImplementationOnce(() => {
+    return Promise.resolve(result);
+  });
+
+  render(<TitleDetails />);
+
+  await waitFor(() => {
+    const resultTitle = screen.getByText(/NGL931799/i);
+    expect(resultTitle).toBeInTheDocument();
+  });
+});
+
+test("should redirect to all titles screen on back button click", async () => {
+  render(<TitleDetails />);
+  const button = screen.getByTestId("backBtn");
+  userEvent.click(button);
+
+  await waitFor(() => {
+    const allTitles = screen.getByText(/All Titles/i);
+    expect(allTitles).toBeInTheDocument();
+  });
+});
+
+test('should render a maps container on the screen', () => {
+    const { container } = render(<TitleDetails />);
+    const titleMapContainer = container.getElementsByClassName(
+      "titleMapContainer"
+    );
+    expect(titleMapContainer.length).toBe(1);
 })
-
-// test('should render title details data on the screen', () => {
-//     const mockTitlesData = jest.mocked(fetchTitles)
-//     mockTitlesData.mockImplementationOnce(() => {
-//         return Promise.resolve(result)
-//     })
-  
-//     render(<Titles />);
-  
-//     await waitFor(() => {
-//       const resultTitle = screen.getByText(/NGL931799/i);
-//       expect(resultTitle).toBeInTheDocument();
-//     });
-// })
-
-// test('should render a back button on the screen', () => { 
-
-//  })
-
-export {}
+export {};
