a18065f61685af8a409f8fbfadfae347b07a21d9
Hani <h.samir@edu.salford.ac.uk>
2023-01-28 00:04:13 +0000

    unit test PASSES: data fetcher for titles

--- src/data/dataFetcher/index.test.ts
+++ src/data/dataFetcher/index.test.ts
@@ -2,18 +2,18 @@ import { fetchTitles } from "."
 import fetchTitlesData from "../fakeData"
 
 jest.mock('../fakeData')
-
+const result = [{
+    "Title Number": "NGL931799",
+    "Property Address": "Lower Ground Floor, 36-38 Hatton Garden, London (EC1N 8EB)",
+    "Tenure": "Leasehold",
+    "X": -0.108098777,
+    "Y": 51.5201911
+}]
 const mockFakeAPI = () => {
-    const mockTitlesData = jest.mocked(fetchTitlesData)
-    return mockTitlesData.mockResolvedValue(() => {
-        [{
-            "Title Number": "NGL931799",
-            "Property Address": "Lower Ground Floor, 36-38 Hatton Garden, London (EC1N 8EB)",
-            "Tenure": "Leasehold",
-            "X": -0.108098777,
-            "Y": 51.5201911
-        }]
-    })
+ const mockTitlesData = jest.mocked(fetchTitlesData)
+ mockTitlesData.mockImplementationOnce(() => {
+    return Promise.resolve(result)
+ })
 }
 test('datafetcher returns an object', async () => { 
     mockFakeAPI()
--- src/data/dataFetcher/index.ts
+++ src/data/dataFetcher/index.ts
@@ -1,1 +1,13 @@
-export const fetchTitles = () => {}
+import { Title } from "../../models/Title"
+import { mapToTitlesData } from "../../utils/MapToTitles"
+import fetchTitlesData from "../fakeData"
+
+export const fetchTitles = async () => {
+    const results = await fetchTitlesData()
+    let titlesData: Title[] = []
+    if(results) {
+        titlesData = [...mapToTitlesData(results)]
+    }
+
+    return titlesData
+}
