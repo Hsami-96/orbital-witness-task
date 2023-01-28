aa065e3f52809c898655ea320421490e8f92e248
Hani <h.samir@edu.salford.ac.uk>
2023-01-28 00:47:59 +0000

    unit test FAILS: added react router dom fails the current test in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import TitlesPage from './pages/Titles';
+import TitleDetailsPage from './pages/TitleDetails';
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TitlesPage />} />
+        <Route path="/title/:number" element={<TitleDetailsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
